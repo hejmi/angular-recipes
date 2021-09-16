5a1e9900b4ec168aa828b4beef0469b3ffb1280e
MichMatch <michael@graphicdesire.se>
2021-09-16 11:20:11 +0200

    working on currentRecipe in recipe-details

--- src/app/components/recipe-details/recipe-details.component.ts
+++ src/app/components/recipe-details/recipe-details.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/models/recipe.model';
+import { RecipesService } from 'src/app/services/recipes.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-details',
   templateUrl: './recipe-details.component.html',
-  styleUrls: ['./recipe-details.component.css']
+  styleUrls: ['./recipe-details.component.css'],
 })
 export class RecipeDetailsComponent implements OnInit {
+  currentRecipe: Recipe = {};
 
-  constructor() { }
+  constructor(
+    private recipeService: RecipesService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.getRecipe(this.route.snapshot.params.id);
   }
 
+  getRecipe(id: number): void {
+    this.recipeService.get(id).subscribe(
+      (data) => {
+        this.currentRecipe = data;
+        console.log(data);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
